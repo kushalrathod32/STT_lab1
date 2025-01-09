f0c84d182f9ee9569e8c35a06bd72baa1a60c148
kushalrathod32 <kushal.rathod@iitgn.ac.in>
2025-01-09 11:03:57 +0530

    edit2

--- a.py
+++ a.py
@@ -1,4 +1,34 @@
-a=10
-b=5
-print(a+b)
+"""first code"""
+A=10
+B=5
+C=A+B
+print(A+B)
+if C>10:
+    print("sum is greater than 10")
+elif C<=10:
+    print("sum is smaller tha or equal to 10")
+D=10
+E=5
+F=D+E
+print(F)
+if F>10:
+    print("sum is greater than 10")
+elif F<=10:
+    print("sum is smaller tha or equal to 10")
+G=10
+H=5
+I=G+H
+print(I)
+if I>10:
+    print("sum is greater than 10")
+elif I<=10:
+    print("sum is smaller tha or equal to 10")
+J=10
+K=5
+L=J+K
+print(L)
+if L>10:
+    print("sum is greater than 10")
+elif L<=10:
+    print("sum is smaller tha or equal to 10")
 #first comment
